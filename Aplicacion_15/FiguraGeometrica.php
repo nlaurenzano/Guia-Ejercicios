bbc5e132ea4386a7317fa40724043efdfb1bece1
nlaurenzano <nlaurenzano@gmail.com>
2016-08-23 21:53:09 -0300

    Algo de herencia.

---
+++ Aplicacion_15/FiguraGeometrica.php
@@ -0,0 +1,66 @@
+<?php
+
+
+/**
+* 
+*/
+public class FiguraGeometrica {
+
+	protected $_color;
+	protected $_perimetro;
+	protected $_superficie;
+
+
+	//function __construct(argument)
+	function FiguraGeometrica() {
+		
+	}
+
+	public function getColor() {
+		return $this=>_color;
+	}
+	public function setColor($color) {
+		$this=>_color = $color;
+	}
+
+	public function ToString() {
+		echo "Color: ".$this=>_color;
+	}
+
+	public function Dibujar() {
+	/*Dibujar, retornará un string (con el color que corresponda) 
+	formando la figura geométrica del objeto que lo invoque 
+	(retornar una serie de asteriscos que modele el objeto). 
+	*/
+
+	return "";
+	}
+
+	protected function CalcularDatos() {
+
+	}
+
+
+
+}
+
+	public class Rectangulo extends FiguraGeometrica {
+
+		private $_ladoDos;
+		private $_ladoUno;
+
+		public function __construct($ladoUno, $ladoDos) {
+			$_ladoUno = $ladoUno;
+			$_ladoDos = $ladoDos;
+			CalcularDatos();
+		}
+
+		private function CalcularDatos() {
+			$_superficie = $_ladoUno * $_ladoDos;
+			$_perimetro = 2 * ($_ladoUno + $_ladoDos);
+		}
+
+
+	}
+
+¿>
---
+++ Aplicacion_15/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Aplicación Nro. 15</title>
+</head>
+<body>
+
+	<?php
+	/* 	Aplicación Nº 15 (Figuras geométricas) 
+	La clase FiguraGeometrica posee: todos sus atributos protegidos, un constructor por defecto, un método getter y setter para el atributo _color, 
+	un método virtual (ToString) y dos métodos abstractos: Dibujar (público) y CalcularDatos (protegido).
+	CalcularDatos será invocado en el constructor de la clase derivada que corresponda, su funcionalidad será la de inicializar 
+	los atributos _superficie y _perimetro.
+	Dibujar, retornará un string (con el color que corresponda) formando la figura geométrica del objeto que lo invoque 
+	(retornar una serie de asteriscos que modele el objeto). 
+	*/
+
+	include "FiguraGeometrica";
+
+	$rectangulo = new Rectangulo(3,4);
+
+	
+	?>
+
+</body>
+</html>
